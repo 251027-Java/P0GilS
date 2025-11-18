a54c6778b5cf0538cedf219d5671be4393b3eaaf
Sidney Gil <sidneygil@Sidneys-MacBook-Air.local>
2025-11-18 11:41:59 -0600

    modified Card.java

--- src/main/java/org/example/model/Card.java
+++ src/main/java/org/example/model/Card.java
@@ -1,15 +1,16 @@
 package org.example.model;
 
 public class Card {
-    //Fields
+    // Fields
     private String id;
     private String name;
     private String rarity;
     private String cardType;
     private String setId;
 
-    // Constructor
+    // Constructors
     public Card() {}
+
     public Card(String id, String name, String rarity, String cardType, String setId) {
         this.id = id;
         this.name = name;
@@ -18,5 +19,30 @@ public class Card {
         this.setId = setId;
     }
 
-    // Methods
+    // Getters / Setters
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getRarity() { return rarity; }
+    public void setRarity(String rarity) { this.rarity = rarity; }
+
+    public String getCardType() { return cardType; }
+    public void setCardType(String cardType) { this.cardType = cardType; }
+
+    public String getSetId() { return setId; }
+    public void setSetId(String setId) { this.setId = setId; }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", rarity='" + rarity + '\'' +
+                ", cardType='" + cardType + '\'' +
+                ", setId='" + setId + '\'' +
+                '}';
+    }
 }
